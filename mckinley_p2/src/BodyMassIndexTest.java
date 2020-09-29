444d2e4e949b389f21cf2d3826fca7976a19e2c0
Duncan <a@b.c>
2020-09-28 20:42:56 -0400

    Fixed the tests, last commit

--- mckinley_p2/src/BodyMassIndexTest.java
+++ mckinley_p2/src/BodyMassIndexTest.java
@@ -7,43 +7,43 @@ public class BodyMassIndexTest
         @Test
         void testBmiCalculatorMethod()
         {
-            BodyMassIndex bmi = new BodyMassIndex(60, 126);
-            assertEquals(24.61, bmi.bmiMath(60, 126));
+            BodyMassIndex bmi = new BodyMassIndex(76, 200);
+            assertEquals(24.34, bmi.bmiMath(76, 200));
         }
         @Test
         void testCategorizeMethod()
 
         {
-            BodyMassIndex cat = new BodyMassIndex(76, 250);
-            assertEquals("Underweight", cat.Categorize(17));
+            BodyMassIndex cat = new BodyMassIndex(76, 100);
+            assertEquals("Underweight", cat.Categorize(cat.bmi));
         }
         @Test
         void testCategory1()
 
         {
-            BodyMassIndex cat1 = new BodyMassIndex(76, 250);
-            assertEquals("Underweight", cat1.Categorize(18.4));
+            BodyMassIndex cat1 = new BodyMassIndex(76, 100);
+            assertEquals("Underweight", cat1.Categorize(cat1.bmi));
         }
         @Test
         void testCategory2()
 
         {
-            BodyMassIndex cat2 = new BodyMassIndex(76, 250);
-            assertEquals("Normal weight", cat2.Categorize(18.5));
+            BodyMassIndex cat2 = new BodyMassIndex(76, 200);
+            assertEquals("Normal weight", cat2.Categorize(cat2.bmi));
         }
         @Test
         void testCategory3()
 
         {
-            BodyMassIndex cat3 = new BodyMassIndex(76, 250);
-            assertEquals("Overweight", cat3.Categorize(25));
+            BodyMassIndex cat3 = new BodyMassIndex(76, 240);
+            assertEquals("Overweight", cat3.Categorize(cat3.bmi));
         }
         @Test
         void testCategory4()
 
         {
-            BodyMassIndex cat = new BodyMassIndex(76, 250);
-            assertEquals("Obesity", cat.Categorize(30));
+            BodyMassIndex cat4 = new BodyMassIndex(76, 250);
+            assertEquals("Obesity", cat4.Categorize(cat4.bmi));
         }
     }
 
